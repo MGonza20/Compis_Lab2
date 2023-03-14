f253dcec7ea956d6bdee35e4b92d1380413c1864
MGonza20 <sara.paguaga@gmail.com>
2023-03-14 12:09:43 -0600

    pruebas - syntax tree

--- thompsonTools/directAFD.py
+++ thompsonTools/directAFD.py
@@ -2,14 +2,15 @@
 from Format import Format
 
 class Node:
-    def __init__(self, symbol, parent = None, enum = None, anulable = False, firstpos = None, lastpos = None):
+    def __init__(self, symbol, parent = None, left = None, right = None, no = None): #, anulable = False, firstpos = None, lastpos = None):
         self.symbol = symbol
         self.parent = parent
-        self.children = []
-        self.enum = enum  
-        self.anulable = anulable
-        self.firstpos = firstpos
-        self.lastpos = lastpos
+        self.left = left
+        self.right = right
+        self.no = no  
+        # self.anulable = anulable
+        # self.firstpos = firstpos
+        # self.lastpos = lastpos
         
 
 class AFD:
@@ -23,36 +24,45 @@ class AFD:
         hashRegex.zeroOrOneId()
         return hashRegex.concat()
     
-    def syntaxTreeFromRegex(self):
-        regex = self.augmentRegex()
-        stack = []
+    def syntaxTree(self):
+        tree = []
+        toDo = []
         enum = 0
+        regex = self.augmentRegex()
+
         for i in range(len(regex)):
+            if len(toDo) > 0:
+                if toDo[-1] == '|':
+                    if len(tree) > 1:
+                        l = tree.pop(0)
+                        r = tree.pop(0)
+                        newSymU = Node(toDo.pop(), left = l, right = r) 
+                        l.parent = newSymU
+                        r.parent = newSymU
+                        tree.append(newSymU)
+                elif toDo[-1] == '.':
+                    if len(tree) > 1:
+                        l = tree.pop(0)
+                        r = tree.pop(0)
+                        newSymC = Node(toDo.pop(), left = l, right = r)
+                        l.parent = newSymC
+                        r.parent = newSymC
+                        tree.append(newSymC)
             if regex[i].isalnum():
-                node = Node(regex[i], enum = enum)
-                stack.append(node)
+                tree.append(Node(regex[i], no = enum))
                 enum += 1
-            elif regex[i] == '.':
-                node = Node(regex[i])
-                node.children.append(stack.pop())
-                node.children.append(stack.pop())
-                stack.append(node)
-            elif regex[i] == '|':
-                node = Node(regex[i])
-                node.children.append(stack.pop())
-                # node.children.append(stack.pop())
-                stack.append(node)
-            elif regex[i] == '*':
-                node = Node(regex[i])
-                node.children.append(stack.pop())
-                stack.append(node)
-            elif regex[i] == '#':
-                node = Node(regex[i])
-                node.children.append(stack.pop())
-                stack.append(node)
-        return stack.pop()
+            elif regex[i] == '|' or regex[i] == '.':
+                if len(tree) < 2:
+                    toDo.append(regex[i])
+            if regex[i] == '*':
+                children = tree.pop(0)
+                newSym = Node(regex[i], left= children)
+                children.parent = newSym
+                tree.append(newSym) 
+        return tree
+
     
 
-afdd = AFD('((12)|b)+abc?')
-print(afdd.augmentRegex())
-
+afdd = AFD('(a|b)+abc?')
+aa = afdd.syntaxTree()
+print(aa)
